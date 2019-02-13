fb1057f0bb250d87f73c9dc1eb38d171cf06d870
Alexander Singleton <alexander.singleton40@gmail.com>
2019-02-13 17:49:22 -0500

    Update constants.js

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,7 +1,7 @@
 import * as firebase from "firebase";
 
 const config = {
-  apiKey: "AIzaSyDrTzX0nVdFUIuBvBA0OldyjRjHmvymIls",
+  apiKey: "",
   authDomain: "add-users-to-app.firebaseapp.com",
   databaseURL: "https://add-users-to-app.firebaseio.com",
   projectId: "add-users-to-app",
